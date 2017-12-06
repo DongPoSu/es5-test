1d2540688fec716b23deda735bc1df01101a46d9
苏政-java <851575021@qq.com>
2017-12-06 17:40:14 +0800

    init

---
+++ grammar.js
@@ -0,0 +1,25 @@
+console.log(typeof 2);
+console.log(typeof "1");
+console.log(typeof true);
+console.log(typeof null)
+console.log(typeof undefined);
+
+
+
+console.log(null == undefined);
+
+if (!undefined) {
+  console.log('undefined is false');
+}
+// undefined is false
+
+if (!null) {
+  console.log('null is false');
+}
+
+
+console.log("1"+undefined);
+console.log("1"+null);
+
+console.log(1+undefined);
+console.log(1+null);
